5ea1e58eb839dc7d7cae20d97b90795ddce28169
Netaki <205346987+Netaki@users.noreply.github.com>
2026-01-17 13:33:48 +0100

    Fix crash on teleporter use

--- src/main/java/com/jarhax/eyespy/api/context/BlockContext.java
+++ src/main/java/com/jarhax/eyespy/api/context/BlockContext.java
@@ -2,17 +2,20 @@ package com.jarhax.eyespy.api.context;
 
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.math.util.ChunkUtil;
 import com.hypixel.hytale.math.vector.Vector3i;
-import com.hypixel.hytale.protocol.BlockPosition;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
-import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
 import com.hypixel.hytale.server.core.universe.world.meta.BlockState;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.util.FillerBlockUtil;
 import com.hypixel.hytale.server.core.util.TargetUtil;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
 import javax.annotation.Nullable;
 
@@ -23,7 +26,7 @@ public class BlockContext extends Context {
     private final Vector3i targetPos;
     private final Vector3i offsetPos;
 
-    public BlockContext(float delta, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer, Player observer, WorldChunk chunk, Vector3i targetPos, Vector3i offsetPos, BlockType block, BlockState state) {
+    public BlockContext(float delta, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer, PlayerRef observer, WorldChunk chunk, Vector3i targetPos, Vector3i offsetPos, BlockType block, BlockState state) {
         super(delta, index, archetypeChunk, store, commandBuffer, observer, chunk);
         this.targetPos = targetPos;
         this.offsetPos = offsetPos;
@@ -49,20 +52,49 @@ public class BlockContext extends Context {
     }
 
     @Nullable
-    public static BlockContext create(Player player, float dt, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer) {
-        final TransformComponent transform = archetypeChunk.getComponent(index, TransformComponent.getComponentType());
-        if (transform != null) {
-            final WorldChunk chunk = transform.getChunk();
-            if (chunk != null) {
-                final Vector3i targetBlockPos = TargetUtil.getTargetBlock(archetypeChunk.getReferenceTo(index), 5, commandBuffer);
-                if (targetBlockPos != null) {
-                    final BlockPosition pos = player.getWorld().getBaseBlock(new BlockPosition(targetBlockPos.x, targetBlockPos.y, targetBlockPos.z));
-                    final BlockType block = chunk.getBlockType(pos.x, pos.y, pos.z);
-                    final BlockState state = chunk.getState(pos.x, pos.y, pos.z);
-                    return new BlockContext(dt, index, archetypeChunk, store, commandBuffer, player, chunk, targetBlockPos, new Vector3i(pos.x, pos.y, pos.z), block, state);
-                }
+    public static BlockContext create(PlayerRef player, float dt, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer) {
+        Ref<EntityStore> playerRef = player.getReference();
+        if (playerRef == null) {
+            return null;
+        }
+        Store<EntityStore> playerStore = playerRef.getStore();
+        TransformComponent transform = playerStore.getComponent(playerRef, TransformComponent.getComponentType());
+        World world = store.getExternalData().getWorld();
+        final Vector3i targetBlockPos = TargetUtil.getTargetBlock(archetypeChunk.getReferenceTo(index), 5, commandBuffer);
+        if (transform != null && targetBlockPos != null) {
+            int x = targetBlockPos.x;
+            int y = targetBlockPos.y;
+            int z = targetBlockPos.z;
+
+            long chunkIndex = ChunkUtil.indexChunkFromBlock(x, z);
+            WorldChunk chunk = world.getChunkIfLoaded(chunkIndex);
+            if (chunk == null) {
+                return null;
             }
+
+            Vector3i basePos = resolveBaseBlock(chunk, x, y, z);
+
+            long baseChunkIndex = ChunkUtil.indexChunkFromBlock(basePos.x, basePos.z);
+            WorldChunk baseChunk = baseChunkIndex == chunkIndex ? chunk : world.getChunkIfLoaded(baseChunkIndex);
+
+            if (baseChunk == null) {
+                return null;
+            }
+
+            BlockType block = baseChunk.getBlockType(basePos.x, basePos.y, basePos.z);
+            BlockState state = baseChunk.getState(basePos.x, basePos.y, basePos.z);
+
+            return new BlockContext(dt, index, archetypeChunk, store, commandBuffer, player, baseChunk, targetBlockPos, basePos, block, state);
         }
         return null;
+    }
+
+    public static Vector3i resolveBaseBlock(WorldChunk chunk, int x, int y, int z) {
+        int filler = chunk.getFiller(x, y, z);
+        if (filler == 0) {
+            return new Vector3i(x, y, z);
+        }
+
+        return new Vector3i(x - FillerBlockUtil.unpackX(filler), y - FillerBlockUtil.unpackY(filler), z - FillerBlockUtil.unpackZ(filler));
     }
 }
--- src/main/java/com/jarhax/eyespy/api/context/EntityContext.java
+++ src/main/java/com/jarhax/eyespy/api/context/EntityContext.java
@@ -4,8 +4,11 @@ import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.math.util.ChunkUtil;
+import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.util.TargetUtil;
@@ -16,7 +19,7 @@ public class EntityContext extends Context {
 
     private final Ref<EntityStore> entity;
 
-    public EntityContext(float delta, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer, Player observer, WorldChunk chunk, Ref<EntityStore> entity) {
+    public EntityContext(float delta, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer, PlayerRef observer, WorldChunk chunk, Ref<EntityStore> entity) {
         super(delta, index, archetypeChunk, store, commandBuffer, observer, chunk);
         this.entity = entity;
     }
@@ -26,12 +29,26 @@ public class EntityContext extends Context {
     }
 
     @Nullable
-    public static EntityContext create(Player player, float dt, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer) {
-
+    public static EntityContext create(PlayerRef player, float dt, int index, ArchetypeChunk<EntityStore> archetypeChunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer) {
+        Ref<EntityStore> playerRef = player.getReference();
+        if (playerRef == null) {
+            return null;
+        }
+        Store<EntityStore> playerStore = playerRef.getStore();
+        World world = store.getExternalData().getWorld();
         final Ref<EntityStore> targetEntity = TargetUtil.getTargetEntity(archetypeChunk.getReferenceTo(index), commandBuffer);
-        final TransformComponent transform = archetypeChunk.getComponent(index, TransformComponent.getComponentType());
-        if (transform != null && targetEntity != null) {
-            final WorldChunk chunk = transform.getChunk();
+        TransformComponent transform = playerStore.getComponent(playerRef, TransformComponent.getComponentType());
+
+        if (transform == null) {
+            return null;
+        }
+        Vector3d position = transform.getPosition();
+
+        if (targetEntity != null) {
+            int X = (int) Math.floor(position.x);
+            int Z = (int) Math.floor(position.z);
+            long chunkIndex = ChunkUtil.indexChunkFromBlock(X, Z);
+            WorldChunk chunk = world.getChunkIfLoaded(chunkIndex);
             return new EntityContext(dt, index, archetypeChunk, store, commandBuffer, player, chunk, targetEntity);
         }
 

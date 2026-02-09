8302f50b8f29c95c5d38f62b93e1d626faebdc76
Tyler Hancock <darklime@live.ca>
2026-02-08 20:14:35 -0700

    Removed unneeded reflection code.

--- src/main/java/com/jarhax/eyespy/impl/util/Reflect.java
+++ src/main/java/com/jarhax/eyespy/impl/util/Reflect.java
@@ -1,24 +1,14 @@
 package com.jarhax.eyespy.impl.util;
 
-import com.hypixel.hytale.codec.Codec;
 import com.hypixel.hytale.server.core.entity.entities.player.hud.CustomUIHud;
 import com.hypixel.hytale.server.core.entity.entities.player.hud.HudManager;
-import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
 import com.jarhax.eyespy.EyeSpy;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
-import java.util.Map;
 import java.util.function.BiConsumer;
-import java.util.function.Supplier;
 
 public class Reflect {
-
-    public static class UICommandBuilder_ {
-        private static final MethodHandles.Lookup LOOKUP = lookup(UICommandBuilder.class);
-        private static final VarHandle _CODEC_MAP = handleStatic(LOOKUP, UICommandBuilder.class, "CODEC_MAP", Map.class);
-        public static final Supplier<Map<Class<?>, Codec<?>>> CODEC_MAP = () -> (Map<Class<?>, Codec<?>>) _CODEC_MAP.get();
-    }
 
     public static class HudManager_ {
         private static final MethodHandles.Lookup LOOKUP = lookup(HudManager.class);
@@ -32,16 +22,6 @@ public class Reflect {
         }
         catch (NoSuchFieldException | IllegalAccessException e) {
             EyeSpy.LOGGER.atSevere().withCause(e).log("Unable to access field '%s' for class '%s'!", name, clazz.getCanonicalName());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static VarHandle handleStatic(MethodHandles.Lookup lookup, Class<?> clazz, String name, Class<?> type) {
-        try {
-            return lookup.findStaticVarHandle(clazz, name, type);
-        }
-        catch (NoSuchFieldException | IllegalAccessException e) {
-            EyeSpy.LOGGER.atSevere().withCause(e).log("Unable to access static field '%s' for class '%s'!", name, clazz.getCanonicalName());
             throw new RuntimeException(e);
         }
     }
